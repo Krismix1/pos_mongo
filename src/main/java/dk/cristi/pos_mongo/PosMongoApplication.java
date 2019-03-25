65aa1f2545a7c8eaed1ff2c1b6ae390dfcaa1422
Epharas <cris0922@stud.kea.dk>
2019-03-25 10:23:29 +0100

    Initial commit

---
+++ src/main/java/dk/cristi/pos_mongo/PosMongoApplication.java
@@ -0,0 +1,13 @@
+package dk.cristi.pos_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosMongoApplication.class, args);
+    }
+
+}
